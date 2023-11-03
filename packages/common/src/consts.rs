9a9ecb53a083b794778186a41e5259db0f352eb3
mennatabuelnaga <mennat.abuelnaga@gmail.com>
2023-11-03 15:23:35 +0200

    refactor: define const ZERO_HASH

--- packages/common/src/consts.rs
+++ packages/common/src/consts.rs
@@ -1,2 +1,5 @@
+use crate::types::Hash;
+
 pub const INITIAL_MINIMUM_STAKE_TO_REGISTER: u128 = 1;
 pub const INITIAL_MINIMUM_STAKE_FOR_COMMITTEE_ELIGIBILITY: u128 = 1;
+pub const ZERO_HASH: Hash = [0; 32];
