be4ad982f66e359d487670865b1e03e48cb88905
gluax <16431709+gluax@users.noreply.github.com>
2024-05-23 16:46:32 -0700

    fix: fmting

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -12,8 +12,8 @@ mod utils;
 #[path = ""]
 #[cfg(test)]
 pub(crate) mod test {
-    pub mod test_helpers;
     mod test_executor;
+    pub mod test_helpers;
     pub use test_executor::TestExecutor;
 
     mod config_test;
