d7b24ed92ead3def151cf4c832804c3bc5b994a2
gluax <16431709+gluax@users.noreply.github.com>
2024-08-19 12:24:00 -0700

    chore: update common

--- contract/src/state.rs
+++ contract/src/state.rs
@@ -1,4 +1,4 @@
-use cosmwasm_std::{StdResult, Storage};
+use cosmwasm_std::{StdResult, Storage, Uint128};
 use cw_storage_plus::{Item, Map};
 
 use crate::types::PublicKey;
@@ -17,8 +17,8 @@ pub fn get_seq(store: &dyn Storage, public_key: &PublicKey) -> StdResult<u128> {
     ACCOUNT_SEQ.may_load(store, public_key).map(|x| x.unwrap_or_default())
 }
 
-pub fn inc_get_seq(store: &mut dyn Storage, public_key: &PublicKey) -> StdResult<u128> {
+pub fn inc_get_seq(store: &mut dyn Storage, public_key: &PublicKey) -> StdResult<Uint128> {
     let seq = ACCOUNT_SEQ.may_load(store, public_key)?.unwrap_or_default();
     ACCOUNT_SEQ.save(store, public_key, &(seq + 1))?;
-    Ok(seq)
+    Ok(seq.into())
 }
