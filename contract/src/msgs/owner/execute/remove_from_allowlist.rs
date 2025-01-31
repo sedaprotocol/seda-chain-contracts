13e7448816ef12f7116c06814291d9348c6cacc9
gluax <16431709+gluax@users.noreply.github.com>
2025-01-31 08:52:47 -0800

    fix: don't overwrite reward pool but add to it

--- contract/src/msgs/owner/execute/remove_from_allowlist.rs
+++ contract/src/msgs/owner/execute/remove_from_allowlist.rs
@@ -12,7 +12,6 @@ impl ExecuteHandler for execute::remove_from_allowlist::Execute {
 
         // we need to remove the address from the allowlist
         let public_key = PublicKey::from_hex_str(&self.public_key)?;
-        ALLOWLIST.remove(deps.storage, &public_key);
 
         if let Some(staker) = STAKERS.may_get_staker(deps.storage, &public_key)? {
             // we move their staked tokens to the pending withdrawal
@@ -20,13 +19,14 @@ impl ExecuteHandler for execute::remove_from_allowlist::Execute {
             let staker = Staker {
                 memo:                      staker.memo,
                 tokens_staked:             Uint128::new(0),
-                tokens_pending_withdrawal: staker.tokens_staked,
+                tokens_pending_withdrawal: staker.tokens_staked.checked_add(staker.tokens_pending_withdrawal)?,
             };
 
             STAKERS.update(deps.storage, public_key.clone(), &staker)?;
         }
 
-        // Move
+        // do this at the end in case we fail above
+        ALLOWLIST.remove(deps.storage, &public_key);
 
         Ok(Response::new()
             .add_attribute("action", "remove-from-allowlist")
