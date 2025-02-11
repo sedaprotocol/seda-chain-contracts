a43d2c8b17c1a6cd086a927fff731287c9609fbd
gluax <16431709+gluax@users.noreply.github.com>
2025-02-11 15:20:32 -0800

    feat: handle errors in processing dist msgs

--- contract/src/msgs/data_requests/sudo/remove_requests.rs
+++ contract/src/msgs/data_requests/sudo/remove_requests.rs
@@ -1,11 +1,10 @@
 use std::collections::HashSet;
 
-use cosmwasm_std::{to_json_string, BankMsg, Coin, DepsMut, Env, Event, Response, Uint128};
+use cosmwasm_std::{to_json_binary, BankMsg, Coin, DepsMut, Env, Event, Response, Uint128};
 use seda_common::{
     msgs::data_requests::sudo::{remove_requests, DistributionMessage},
     types::{Hash, ToHexStr},
 };
-use serde_json::json;
 
 use super::{ContractError, SudoHandler};
 use crate::{
@@ -36,20 +35,54 @@ fn burn(amount: Uint128, token: &str, escrow: &mut Escrow) -> BankMsg {
     }
 }
 
+// TODO: use this everywhere we used to build json strings
+macro_rules! json_str {
+    ($( $key:tt : $value:expr ),* $(,)?) => {
+        format!(
+            "{{ {} }}",
+            [ $( format!("\"{}\": \"{}\"", $key, $value) ),* ].join(", ")
+        )
+    };
+}
+
 fn remove_request_and_process_distributions(
     dr_id_str: String,
     messages: &[DistributionMessage],
     deps: &mut DepsMut,
     token: &str,
-) -> Result<(Event, Vec<BankMsg>, HashSet<PublicKey>), ContractError> {
-    // find the data request from the committed pool (if it exists, otherwise error)
-    let dr_id = Hash::from_hex_str(&dr_id_str)?;
-    let dr = state::load_request(deps.storage, &dr_id)?;
+    minimum_stake: &Uint128,
+) -> (Event, Vec<BankMsg>, HashSet<PublicKey>, u8) {
+    let mut event = Event::new("seda-remove-dr");
 
-    let mut event = Event::new("seda-remove-dr")
-        .add_attributes([("dr_id", dr_id_str), ("posted_dr_height", dr.height.to_string())]);
+    let Ok(dr_id) = Hash::from_hex_str(&dr_id_str) else {
+        return (
+            event.add_attribute("invalid_dr_id", dr_id_str),
+            vec![],
+            HashSet::new(),
+            1,
+        );
+    };
+    let Ok(dr) = state::load_request(deps.storage, &dr_id) else {
+        return (
+            event.add_attribute("dr_not_found", dr_id_str),
+            vec![],
+            HashSet::new(),
+            2,
+        );
+    };
+    let Ok(mut dr_escrow) = DR_ESCROW.load(deps.storage, &dr_id) else {
+        return (
+            event.add_attribute("dr_escrow_not_found", dr_id_str),
+            vec![],
+            HashSet::new(),
+            3,
+        );
+    };
 
-    let mut dr_escrow = DR_ESCROW.load(deps.storage, &dr_id)?;
+    event = event.add_attributes([
+        ("dr_id", dr_id_str.clone()),
+        ("posted_dr_height", dr.height.to_string()),
+    ]);
 
     // add 1 so we can account for the refund message that may be sent
     let mut bank_messages = Vec::new();
@@ -69,12 +102,7 @@ fn remove_request_and_process_distributions(
             DistributionMessage::Burn(distribution_burn) => {
                 let amount_to_burn = distribution_burn.amount.min(dr_escrow.amount);
                 bank_messages.push(burn(amount_to_burn, token, &mut dr_escrow));
-                event = event.add_attribute(
-                    "burn",
-                    to_json_string(&json!({
-                        "amount": amount_to_burn,
-                    }))?,
-                );
+                event = event.add_attribute("burn", json_str!("amount": amount_to_burn));
             }
             DistributionMessage::DataProxyReward(distribution_send) => {
                 let amount_to_reward = distribution_send.amount.min(dr_escrow.amount);
@@ -88,19 +116,19 @@ fn remove_request_and_process_distributions(
 
                     event = event.add_attribute(
                         "data_proxy_reward",
-                        to_json_string(&json!({
+                        json_str!(
                             "amount": amount_to_reward,
                             "payout_address": distribution_send.payout_address,
-                        }))?,
+                        ),
                     );
                 } else {
                     bank_messages.push(burn(amount_to_reward, token, &mut dr_escrow));
                     event = event.add_attribute(
                         "data_proxy_reward_invalid_address",
-                        to_json_string(&json!({
+                        json_str!(
                             "payout_address": distribution_send.payout_address,
                             "burn_amount": amount_to_reward,
-                        }))?,
+                        ),
                     );
                 }
             }
@@ -111,10 +139,10 @@ fn remove_request_and_process_distributions(
                     bank_messages.push(burn(amount_to_reward, token, &mut dr_escrow));
                     event = event.add_attribute(
                         "executor_reward_invalid_identity",
-                        to_json_string(&json!({
+                        json_str!(
                             "identity": distribution_executor_reward.identity,
                             "burn_amount": amount_to_reward,
-                        }))?,
+                        ),
                     );
                     continue 'process_message;
                 };
@@ -123,16 +151,15 @@ fn remove_request_and_process_distributions(
                     bank_messages.push(burn(amount_to_reward, token, &mut dr_escrow));
                     event = event.add_attribute(
                         "executor_reward_invalid_identity",
-                        to_json_string(&json!({
+                        json_str!(
                             "identity": distribution_executor_reward.identity,
                             "burn_amount": amount_to_reward,
-                        }))?,
+                        ),
                     );
                     continue 'process_message;
                 };
 
-                let minimum_stake = STAKING_CONFIG.load(deps.storage)?.minimum_stake_to_register;
-                let (remaining_reward, topped_up) = if staker.tokens_staked < minimum_stake {
+                let (remaining_reward, topped_up) = if &staker.tokens_staked < minimum_stake {
                     // top the staker up to minimum stake from the amount in the reward & escrow
                     let top_up = minimum_stake.saturating_sub(staker.tokens_staked);
                     let top_up = top_up.min(amount_to_reward);
@@ -148,16 +175,21 @@ fn remove_request_and_process_distributions(
                 // send remaining reward to the staker pending withdrawal
                 staker.tokens_pending_withdrawal += remaining_reward;
                 dr_escrow.amount = dr_escrow.amount.saturating_sub(remaining_reward);
-                STAKERS.update(deps.storage, public_key.clone(), &staker)?;
+
+                if STAKERS.update(deps.storage, public_key.clone(), &staker).is_err() {
+                    todo!("what should we do here?");
+                    // continue 'process_message;
+                };
+
                 stakers_effected.insert(public_key);
 
                 event = event.add_attribute(
                     "executor_reward",
-                    to_json_string(&json!({
+                    json_str!(
                         "amount": remaining_reward,
                         "topped_up": topped_up,
                         "identity": distribution_executor_reward.identity,
-                    }))?,
+                    ),
                 );
             }
         }
@@ -171,26 +203,33 @@ fn remove_request_and_process_distributions(
         event = event.add_attribute("refund", dr_escrow.amount.to_string());
     }
 
-    state::remove_request(deps.storage, dr_id)?;
+    if state::remove_request(deps.storage, dr_id).is_err() {
+        event = event.add_attribute("failed_to_remove_dr", dr_id_str);
+    };
     DR_ESCROW.remove(deps.storage, &dr_id);
 
-    Ok((event, bank_messages, stakers_effected))
+    (event, bank_messages, stakers_effected, 0)
 }
 
 impl SudoHandler for remove_requests::Sudo {
     fn sudo(self, mut deps: DepsMut, _: Env) -> Result<Response, ContractError> {
         let token = TOKEN.load(deps.storage)?;
+        let minimum_stake = STAKING_CONFIG.load(deps.storage)?.minimum_stake_to_register;
         let mut response = Response::new();
 
         let mut all_stakers_effected = HashSet::new();
-        for removal in self
-            .requests
-            .into_iter()
-            .map(|(dr_id, messages)| remove_request_and_process_distributions(dr_id, &messages, &mut deps, &token))
+        let mut dr_ids_with_status_codes = Vec::new();
+        for (dr_id, (event, bank_messages, stakers_effected, status_code)) in
+            self.requests.into_iter().map(|(dr_id, messages)| {
+                (
+                    dr_id.clone(),
+                    remove_request_and_process_distributions(dr_id, &messages, &mut deps, &token, &minimum_stake),
+                )
+            })
         {
-            let (event, bank_messages, stakers_effected) = removal?;
             all_stakers_effected.extend(stakers_effected);
             response = response.add_event(event).add_messages(bank_messages);
+            dr_ids_with_status_codes.push((dr_id, status_code));
         }
 
         for staker in all_stakers_effected {
@@ -200,6 +239,6 @@ impl SudoHandler for remove_requests::Sudo {
             ));
         }
 
-        Ok(response)
+        Ok(response.set_data(to_json_binary(&dr_ids_with_status_codes)?))
     }
 }
