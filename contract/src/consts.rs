18280631ee8fb382d2c37f06fabde9daed326844
Thomas van Dam <thomas@seda.xyz>
2025-05-23 13:24:59 +0200

    chore(contract): update default contract setting

--- contract/src/consts.rs
+++ contract/src/consts.rs
@@ -2,8 +2,8 @@ use std::num::NonZero;
 
 use cosmwasm_std::Uint128;
 
-pub const INITIAL_MINIMUM_STAKE: Uint128 = Uint128::new(10);
+pub const INITIAL_MINIMUM_STAKE: Uint128 = Uint128::new(10_000_000_000_000_000_000_000);
 
-pub const INITIAL_COMMIT_TIMEOUT_IN_BLOCKS: u64 = 10;
+pub const INITIAL_COMMIT_TIMEOUT_IN_BLOCKS: u64 = 50;
 pub const INITIAL_REVEAL_TIMEOUT_IN_BLOCKS: u64 = 5;
 pub const INITIAL_BACKUP_DELAY_IN_BLOCKS: NonZero<u64> = NonZero::new(2).unwrap();
