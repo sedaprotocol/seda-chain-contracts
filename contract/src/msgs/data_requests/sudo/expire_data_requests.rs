036408743574869955efe43317a9b2076d2f5a57
Mario Cao <mariocao@gmail.com>
2025-06-13 15:20:39 +0200

    DO NOT MERGE 2 (commits)

--- contract/src/msgs/data_requests/sudo/expire_data_requests.rs
+++ contract/src/msgs/data_requests/sudo/expire_data_requests.rs
@@ -22,50 +22,69 @@ impl SudoHandler for expire_data_requests::Sudo {
             dr_id[0] = (env.block.height % 255) as u8;
             dr_id[1] = i as u8;
             let mut dr = DataRequestContract {
-                base: DataRequestBase {
-                    id: dr_id.to_hex(),
-                    version: Version::new(0, 0, 0),
-                    exec_program_id: "a".repeat(64).to_string(),
-                    exec_inputs: "x"
+                base:    DataRequestBase {
+                    id:                 dr_id.to_hex(),
+                    version:            Version::new(0, 0, 0),
+                    exec_program_id:    "a".repeat(64).to_string(),
+                    exec_inputs:        "x"
                         .repeat(INITIAL_DR_CONFIG.exec_input_limit_in_bytes.get() as usize)
                         .as_bytes()
                         .into(),
-                    exec_gas_limit: 0,
-                    tally_program_id: "b".repeat(64).to_string(),
-                    tally_inputs: "y".repeat(INITIAL_DR_CONFIG.tally_input_limit_in_bytes.get() as usize).as_bytes().into(),
-                    tally_gas_limit: 0,
-                    replication_factor: 1,
-                    consensus_filter: "z".repeat(INITIAL_DR_CONFIG.consensus_filter_limit_in_bytes.get() as usize).as_bytes().into(),
-                    gas_price: Uint128::from(0u128),
-                    memo: "m".repeat(INITIAL_DR_CONFIG.memo_limit_in_bytes.get() as usize).as_bytes().into(),
-                    payback_address: "o".repeat(INITIAL_DR_CONFIG.payback_address_limit_in_bytes.get() as usize).as_bytes().into(),
-                    seda_payload: "p".repeat(INITIAL_DR_CONFIG.seda_payload_limit_in_bytes.get() as usize).as_bytes().into(),
-                    commits: Default::default(),
-                    height: 0,
+                    exec_gas_limit:     0,
+                    tally_program_id:   "b".repeat(64).to_string(),
+                    tally_inputs:       "y"
+                        .repeat(INITIAL_DR_CONFIG.tally_input_limit_in_bytes.get() as usize)
+                        .as_bytes()
+                        .into(),
+                    tally_gas_limit:    0,
+                    replication_factor: 100,
+                    consensus_filter:   "z"
+                        .repeat(INITIAL_DR_CONFIG.consensus_filter_limit_in_bytes.get() as usize)
+                        .as_bytes()
+                        .into(),
+                    gas_price:          Uint128::from(0u128),
+                    memo:               "m"
+                        .repeat(INITIAL_DR_CONFIG.memo_limit_in_bytes.get() as usize)
+                        .as_bytes()
+                        .into(),
+                    payback_address:    "o"
+                        .repeat(INITIAL_DR_CONFIG.payback_address_limit_in_bytes.get() as usize)
+                        .as_bytes()
+                        .into(),
+                    seda_payload:       "p"
+                        .repeat(INITIAL_DR_CONFIG.seda_payload_limit_in_bytes.get() as usize)
+                        .as_bytes()
+                        .into(),
+                    commits:            Default::default(),
+                    height:             0,
                 },
                 reveals: Default::default(),
             };
 
-            let identity = format!("{}33188bd0a0111b4de8da9ccab0225f72babcd0a46de83e0848f028f87a6efb14f", i);
-            dr.base.commits.insert(identity.clone(), [0; 32]);
-            dr.reveals.insert(identity.clone());
+            for j in 0..100 {
+                let identity = format!("{:02}69880a0111b4de8da9ccab0225f72babcd0a46de83e0848f028f87a6efb14f", j);
+                dr.base.commits.insert(identity.clone(), [0; 32]);
+                dr.reveals.insert(identity.clone());
+            }
             state::post_request(deps.storage, env.block.height, &dr_id, dr.clone())?;
 
-            state::reveal(
-                deps.storage,
-                &dr_id,
-                dr,
-                env.block.height,
-                &identity,
-                RevealBody {
-                    dr_id: dr_id.to_hex(),
-                    dr_block_height: env.block.height,
-                    exit_code: 0,
-                    gas_used: 0,
-                    reveal: "g".repeat(INITIAL_DR_CONFIG.dr_reveal_size_limit_in_bytes.get() as usize).as_bytes().into(),
-                    proxy_public_keys: vec![],
-                },
-            )?;
+            // state::reveal(
+            //     deps.storage,
+            //     &dr_id,
+            //     dr,
+            //     env.block.height,
+            //     &identity,
+            //     RevealBody {
+            //         dr_id: dr_id.to_hex(),
+            //         dr_block_height: env.block.height,
+            //         exit_code: 0,
+            //         gas_used: 0,
+            //         reveal:
+            // "g".repeat(INITIAL_DR_CONFIG.dr_reveal_size_limit_in_bytes.get()
+            // as usize).as_bytes().into(),
+            //         proxy_public_keys: vec![],
+            //     },
+            // )?;
         }
 
         if ids.is_empty() {
