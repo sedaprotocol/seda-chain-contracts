adec1e75362560d4e20febfe28bddcc1c0dc1d82
Thomas van Dam <thomas@seda.xyz>
2025-06-13 15:23:13 +0200

    fix(contract): emit all dr config attributes
    Closes: #300

--- contract/src/msgs/data_requests/execute/dr_events.rs
+++ contract/src/msgs/data_requests/execute/dr_events.rs
@@ -9,5 +9,30 @@ pub fn create_dr_config_event(config: DrConfig) -> Event {
         ("commit_timeout_in_blocks", config.commit_timeout_in_blocks.to_string()),
         ("reveal_timeout_in_blocks", config.reveal_timeout_in_blocks.to_string()),
         ("backup_delay_in_blocks", config.backup_delay_in_blocks.to_string()),
+        (
+            "dr_reveal_size_limit_in_bytes",
+            config.dr_reveal_size_limit_in_bytes.to_string(),
+        ),
+        (
+            "exec_input_limit_in_bytes",
+            config.exec_input_limit_in_bytes.to_string(),
+        ),
+        (
+            "tally_input_limit_in_bytes",
+            config.tally_input_limit_in_bytes.to_string(),
+        ),
+        (
+            "consensus_filter_limit_in_bytes",
+            config.consensus_filter_limit_in_bytes.to_string(),
+        ),
+        ("memo_limit_in_bytes", config.memo_limit_in_bytes.to_string()),
+        (
+            "payback_address_limit_in_bytes",
+            config.payback_address_limit_in_bytes.to_string(),
+        ),
+        (
+            "seda_payload_limit_in_bytes",
+            config.seda_payload_limit_in_bytes.to_string(),
+        ),
     ])
 }
